aa5809581c7b57acf9ccaa2d41af48f9157b66e9
Alex Bezuska <abezuska@gmail.com>
2015-10-31 09:51:05 -0400

    add updated art

---
+++ step-2/README.md
@@ -0,0 +1,5 @@
+### Candy Catch
+a SplatJS game tutorial by Alex Bezuska
+
+
+http://codepen.io/AlexBezuska/pen/WQyKxR
--- step-2/game.js
+++ step-2/game.js
@@ -22,14 +22,18 @@ var assets = {
 };
 var game = new Splat.Game(canvas, assets);
 var score = 0;
+var gameSeconds = 30;
 
 game.scenes.add("title", new Splat.Scene(canvas, function() {
   // setup
+
+  score = 0;
+
   var backgroundImage = game.animations.get("background");
   this.background = new Splat.AnimatedEntity( 0, 0, backgroundImage.width, backgroundImage.height, backgroundImage, 0, 0 );
 
   var playerSprite = game.animations.get("player");
-  this.player = new Splat.AnimatedEntity( 450, 250, playerSprite.width, playerSprite.height, playerSprite, 0, 0 );
+  this.player = new Splat.AnimatedEntity( 450, (canvas.height - playerSprite.height) - 42, playerSprite.width, playerSprite.height, playerSprite, 0, 0 );
   this.player.angle = 0;
 
   var scene = this;
@@ -44,36 +48,44 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
   });
   this.timers.makecandy.start();
 
+  //Game Timer
+  this.timers.gameTimer = new Splat.Timer(undefined, gameSeconds * 1000, function(){
+    game.scenes.switchTo("gameover");
+    this.reset();
+  });
+  this.timers.gameTimer.start();
+
 }, function(time) {
   // simulation
 
   if(game.keyboard.isPressed("left")){
-    rotate(this.player, "left", 1);
+    this.player.vx = -0.3;
+    this.player.move(time);
   }
 
   if(game.keyboard.isPressed("right")){
-    rotate(this.player, "right", 1);
+    this.player.vx = 0.3;
+    this.player.move(time);
   }
 
   this.player.speed = 0;
 
-  if(game.keyboard.isPressed("up")){
-    this.player.move(time);
-    this.player.speed = 0.3;
-  }
-
-  if(game.keyboard.isPressed("down")){
-    this.player.move(time);
-    this.player.speed = -0.1;
-  }
 
   moveForward(this.player, this.player.speed);
 
   for (var i = 0; i < this.candy.length; i++){
+
     if(this.player.collides(this.candy[i])){
       this.candy.splice(i,1);
       score += 1;
     }
+    //garbage collection
+    if(this.candy[i].y > canvas.height){
+      this.candy.splice(i,1);
+    }
+
+    this.candy[i].y += .8;
+
   }
 
   keepOnScreen(this.player);
@@ -90,15 +102,41 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
   context.font = "32px impact";
   centerText(context, "SCORE: " + score, 0, canvas.height - 25);
 
+  context.fillStyle = "#fff";
+  context.font = "32px impact";
+  context.fillText("Time: " + parseInt((this.timers.gameTimer.expireMillis - this.timers.gameTimer.time) / 1000), 20, 50);
+
 }));
 
+
+game.scenes.add("gameover", new Splat.Scene(canvas, function() {
+  // setup
+}, function(time) {
+  // simulation
+  if(game.keyboard.isPressed("enter") || game.keyboard.isPressed("space")){
+    game.scenes.switchTo("title");
+  }
+}, function(context) {
+  // draw
+
+  context.fillStyle = "#000";
+  context.fillRect(0,0, canvas.width, canvas.height);
+
+  context.fillStyle = "#fff";
+  context.font = "32px impact";
+  centerText(context, "Time's up!", 0, canvas.height/2);
+
+  context.fillStyle = "#fff";
+  context.font = "32px impact";
+  centerText(context, "SCORE: " + score, 0, canvas.height - 25);
+
+}));
 
 
 game.scenes.switchTo("loading");
 
 function makecandy(array, sprite) {
-  var candyEntity = new Splat.AnimatedEntity(randomRange(0, canvas.width), randomRange(0, canvas.height), sprite.width, sprite.height, sprite, 0, 0);
-  candyEntity.angle = randomRange(0, 360);
+  var candyEntity = new Splat.AnimatedEntity(randomRange(0, canvas.width), 0, sprite.width, sprite.height, sprite, 0, 0);
   array.push(candyEntity);
 }
 
